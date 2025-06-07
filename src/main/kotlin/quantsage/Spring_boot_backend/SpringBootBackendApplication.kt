4533983d7d121f3e6abbdcf1dcb25b1e53f174b3
sanyammahajan21 <sanyammahajan2003@outlook.com>
2025-06-07 14:09:13 +0530

    Initial Commit

---
+++ src/main/kotlin/quantsage/Spring_boot_backend/SpringBootBackendApplication.kt
@@ -0,0 +1,11 @@
+package quantsage.Spring_boot_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootBackendApplication>(*args)
+}
---
+++ src/test/kotlin/quantsage/Spring_boot_backend/SpringBootBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package quantsage.Spring_boot_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
